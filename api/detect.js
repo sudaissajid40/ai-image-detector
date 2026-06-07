a1d360d7e96cdfc52ba9e09d70ae0c49fb2b3e65
Sudais Sajid <sudaissajid40@gmail.com>
2026-06-07 20:52:54 +0500

    fix: remove generationConfig payload from Gemini request

--- api/detect.js
+++ api/detect.js
@@ -137,10 +137,7 @@ export default async function handler(req, res) {
                   }
                 ]
               }
-            ],
-            generationConfig: {
-              responseMimeType: 'application/json'
-            }
+            ]
           })
         });
 
